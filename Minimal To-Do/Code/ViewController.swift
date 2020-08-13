bc31ee8cc4ad8d3cbf563e5e8e18144e219a3141
Shubham0812 <singh.shubham0812@gmail.com>
2020-08-13 21:55:18 +0530

    Initialized Project

---
+++ Minimal To-Do/Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minimal To-Do
+//
+//  Created by Shubham Singh on 13/08/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
